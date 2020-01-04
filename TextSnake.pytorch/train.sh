c1e1dbc6cc3aa2a14678a4c724ee4958a4bf0447
AndySer37 <andy19970307@gmail.com>
2020-01-04 23:59:46 +0800

    add

--- TextSnake.pytorch/train.sh
+++ TextSnake.pytorch/train.sh
@@ -1,2 +1,2 @@
 EXPNAME=example
-python3.7 train_textsnake.py --dataset synth-text --batch_size 6 $EXPNAME --viz
+python3 train_textsnake.py --num_workers=0 --dataset synth-text --batch_size 6 $EXPNAME --viz
